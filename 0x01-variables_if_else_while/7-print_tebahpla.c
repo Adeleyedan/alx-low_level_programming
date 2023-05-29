dbc1befd01eb11952c4bfbca5c842280bd67cbde
Adeleyedan <adeleyeboluwatife123@gmail.com>
2023-05-29 11:37:51 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print all letters in lowercase in reverse
+ *
+ * Return: 0 (Always)
+ */
+int main(void)
+{
+	char letters;
+
+	for (letters = 'z'; letters = 'a'; letters--)
+	{
+		putchar(letters);
+	}
+	putchar('\n');
+	return (0);
+}
