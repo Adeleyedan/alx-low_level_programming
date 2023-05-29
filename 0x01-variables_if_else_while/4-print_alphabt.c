c2b102534fc97c058fa5f7e36ffa2d8ef7216424
Adeleyedan <adeleyeboluwatife123@gmail.com>
2023-05-29 10:32:37 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (letters = 'a'; letters <= 'z'; letters++)
 	{
-		if (letter != 'q' && letter != 'e')
+		if (letters != 'q' && letters != 'e')
 		{
 			putchar(letters);
 		}
