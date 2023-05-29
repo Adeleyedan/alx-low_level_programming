6dd5ce17e4e3cc6cfa8db3644629d0efea61733f
Adeleyedan <adeleyeboluwatife123@gmail.com>
2023-05-29 11:01:41 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single numbers line by line
+ *
+ * Retun: 0 (Always)
+ */
+int main(void)
+{
+	putchar('0');
+	putchar('\n');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
+	putchar('\n');
+
+	return (0);
+}
