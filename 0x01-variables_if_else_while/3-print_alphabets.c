be7508c5a565b1e9a0956a6c755835129cec3d53
Adeleyedan <adeleyeboluwatife123@gmail.com>
2023-05-29 00:47:00 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print all letters in lowecase and uppercase
+ *
+ * Return: 0 (Always)
+ */
+int main(void)
+{
+	char[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[52]);
+	}
+	putchar('\n');
+	return (0);
+}
