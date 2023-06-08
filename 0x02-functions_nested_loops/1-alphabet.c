9eaa87eeffe1dc6ff779c3b19c44bc6d927d38a3
Adeleyedan <adeleyeboluwatife123@gmail.com>
2023-06-08 11:57:42 -0700

    _putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Print all letters in lowercase
+ *
+ * Return: 0 (Always)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
